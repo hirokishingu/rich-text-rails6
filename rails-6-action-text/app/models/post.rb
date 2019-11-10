fe33505c3ba22eb6a0747ec5d155c4588fced14d
hirokishingu <hrk.bass.526@gmail.com>
2019-11-10 10:57:17 +0900

    Add I18n ja version.

--- rails-6-action-text/app/models/post.rb
+++ rails-6-action-text/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
-  validates :title, length: { maximum: 32 }, presence: true
+  validates :title, length: { maximum: 34 }, presence: true
 end
