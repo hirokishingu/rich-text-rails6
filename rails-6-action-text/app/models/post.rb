b92b051f0c31ad7988815e788df479d7259be2cd
hirokishingu <hrk.bass.526@gmail.com>
2019-11-10 11:22:45 +0900

    Add byte validation in post.

--- rails-6-action-text/app/models/post.rb
+++ rails-6-action-text/app/models/post.rb
@@ -4,8 +4,26 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 34 }, presence: true
 
   validate :validate_content_length
+  validate :validate_content_attachment_byte_size
 
-  MAX_CONTENT_LENGTH = 50
+  MAX_CONTENT_LENGTH = 500
+  ONE_KILOBYTE = 1024
+  MEGA_BYTES = 3
+  MAX_CONTENT_ATTACHEMENT_BYTE_SIZE = MEGA_BYTES * (1_000 * ONE_KILOBYTE)
+
+  private
+
+  def validate_content_attachment_byte_size
+    content.body.attachables.grep(ActiveStorage::Blob).each do |attachable|
+      if attachable.byte_size > MAX_CONTENT_ATTACHEMENT_BYTE_SIZE
+        errors.add(
+          :base,
+          :content_attachment_byte_size_is_too_big,
+          max_content_attachment_mega_byte_size: MEGA_BYTES,
+        )
+      end
+    end
+  end
 
   def validate_content_length
     if content.to_plain_text.length>  MAX_CONTENT_LENGTH
