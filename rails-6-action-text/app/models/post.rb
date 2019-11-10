bbca0dfd8e92bc162321a40e7b117db8e270b907
hirokishingu <hrk.bass.526@gmail.com>
2019-11-10 11:10:27 +0900

    Add content validation in post.

--- rails-6-action-text/app/models/post.rb
+++ rails-6-action-text/app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   validates :title, length: { maximum: 34 }, presence: true
+
+  validate :validate_content_length
+
+  MAX_CONTENT_LENGTH = 50
+
+  def validate_content_length
+    if content.to_plain_text.length>  MAX_CONTENT_LENGTH
+      errors.add(:content, :too_long, max_content_length: MAX_CONTENT_LENGTH, length: content.to_plain_text.length)
+    end
+  end
 end
